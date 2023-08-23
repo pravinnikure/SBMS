517079fc619647bbebad543a9ea4aa3a05b82eff
pravinjenkins <pravinnikure300@gmail.com>
2023-08-23 15:56:55 +0530

    Project Added Initial commit

---
+++ SBMS_SpringBoot/SBMS_Rest_ConsumerApp/src/main/java/com/app/restapi/SbmsRestConsumerAppApplication.java
@@ -0,0 +1,13 @@
+package com.app.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRestConsumerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRestConsumerAppApplication.class, args);
+	}
+
+}
