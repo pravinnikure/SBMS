9943df756d5c0e4a0b5e4bb00567a3af1038f8a9
pravinjenkins <pravinnikure300@gmail.com>
2023-08-24 19:43:22 +0530

    Adding H2 DB demo service

---
+++ SBMS_SpringBoot/SBMS_Rest_CRUD_H2_db/src/main/java/com/app/restapi/SbmsRestCrudH2DbApplication.java
@@ -0,0 +1,13 @@
+package com.app.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRestCrudH2DbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRestCrudH2DbApplication.class, args);
+	}
+
+}
