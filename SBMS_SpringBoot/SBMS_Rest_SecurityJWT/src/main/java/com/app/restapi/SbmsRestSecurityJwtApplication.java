e52ef28565b84ce3def28701955bc04b4cbf3359
pravinjenkins <pravinnikure300@gmail.com>
2023-09-11 12:47:39 +0530

    Adding JWT projects

---
+++ SBMS_SpringBoot/SBMS_Rest_SecurityJWT/src/main/java/com/app/restapi/SbmsRestSecurityJwtApplication.java
@@ -0,0 +1,13 @@
+package com.app.restapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbmsRestSecurityJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsRestSecurityJwtApplication.class, args);
+	}
+
+}
